b139070846e284a3288927ceb4418894b4631be7
joe-barrett <joe.barrett@stfc.ac.uk>
2019-01-23 14:49:47 +0000

    Relocate App component. Replace with original WebJive one

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
-import {Provider} from 'react-redux';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './components/App/App';
 import './index.css';
 
 const store = configureStore();
 
 render(
   <Provider store={store}>
-      <App />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
