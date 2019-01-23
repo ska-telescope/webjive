4bab99f5b2a27711b0621be933ea8c52fd48e898
joe-barrett <joe.barrett@stfc.ac.uk>
2019-01-23 14:56:30 +0000

    Relocate dashboard component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ import DeviceList from '../../DeviceList/DeviceList';
 import DeviceViewer from '../../DeviceViewer/DeviceViewer';
 import HomeViewer from '../../HomeViewer/HomeViewer';
 import ErrorDisplay from '../../ErrorDisplay/ErrorDisplay';
-import Dashboard from '../../Dashboard/Dashboard';
+import Dashboard from '../Dashboard/Dashboard';
 import ModalDialog from '../../Modal/Modal';
 import LogInOut from '../../LogInOut/LogInOut';
 
