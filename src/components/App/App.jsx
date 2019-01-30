a84b215d0587492d1e66cec9706c413fe50b4697
joe-barrett <joe.barrett@stfc.ac.uk>
2019-01-30 08:06:09 +0000

    Move app component to match desired directory structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Layout from './Layout/Layout';
+import Layout from '../../Layout/Layout';
 
 import './App.css';
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
-import App from './App';
+import App from './components/App/App';
 import './index.css';
 
 const store = configureStore();
