77149f0630fb2a4495862c26a764bf4b21f6d515
joe-barrett <joe.barrett@stfc.ac.uk>
2019-02-07 15:07:52 +0000

    Add saving to a custom filename

--- src/components/Modal/SaveCanvases.jsx
+++ src/components/Modal/SaveCanvases.jsx
@@ -3,21 +3,48 @@ import PropTypes from 'prop-types';
 import fileDownload from 'js-file-download';
 import { Button, Modal } from 'react-bootstrap';
 
+const getFilename = () => `webjive-layout-${new Date().getTime()}`;
+
 export default class SaveCanvases extends Component {
+  constructor(props) {
+    super(props);
+    this.filenameInput = React.createRef();
+    this.state = {
+      filename: getFilename()
+    };
+  }
+
+  componentDidMount = () => {
+    this.filenameInput.current.focus();
+  };
+
+  handleFilenameChange = () => {
+    this.setState({ filename: this.filenameInput.current.value });
+  };
+
   handleSaveClick = () => {
     const { canvases, closeDialog } = this.props;
-    fileDownload(canvases, 'webjive.json', 'application/json');
+    const { filename } = this.state;
+    fileDownload(canvases, `${filename}.json`, 'application/json');
     closeDialog();
   };
 
   render() {
     const { closeDialog } = this.props;
+    const { filename } = this.state;
     return (
       <Modal.Dialog>
         <Modal.Header>
           <Modal.Title>Save Canvases</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Test</Modal.Body>
+        <Modal.Body>
+          <input
+            ref={this.filenameInput}
+            className="form-control"
+            placeholder={filename}
+            onChange={this.handleFilenameChange}
+          />
+        </Modal.Body>
         <Modal.Footer>
           <Button className="btn btn-outline-secondary" onClick={this.handleSaveClick}>
             Save
